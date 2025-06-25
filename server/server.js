6e663556e12ae3509156258cc6a21b65e5e9e911
Shashank-Janagam <shashijanagam2004@gmail.com>
2025-06-25 21:14:27 +0530

    f

--- server/server.js
+++ server/server.js
@@ -25,24 +25,6 @@
 //   .catch(err => console.error('❌ MongoDB connection error:', err));
 
 // // ✅ Define allowed origins
-// const allowedOrigins = [
-//   'http://localhost:5173',
-//   'https://getlearnxai.vercel.app'
-// ];
-
-// // ✅ Full CORS config
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
 
 // app.use(cors(corsOptions));
 
@@ -75,6 +57,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://getlearnxai.vercel.app'
+];
+
+// // ✅ Full CORS config
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
 app.get('/', (req, res) => {
   res.send('Minimal server works!');
